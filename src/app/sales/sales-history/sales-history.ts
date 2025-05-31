9f4cecebfbea87b0946e6a00199091b3a55b05d8
Daniel Perez Cortez <pcd510@hotmail.com>
2025-05-31 12:24:31 -0600

    markplace

---
+++ src/app/sales/sales-history/sales-history.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-history',
+  imports: [],
+  templateUrl: './sales-history.html',
+  styleUrl: './sales-history.css'
+})
+export class SalesHistory {
+
+}
